19715deb568d2481990a9b0a61f9fd019a04ccf1
Olga <starusevaos@gmail.com>
2024-03-17 17:50:55 +0100

    Task 1. Project initialization.

---
+++ PdfDataExtract/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PdfDataExtract
+{
+    /// <summary>
+    /// Upload and process files
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
